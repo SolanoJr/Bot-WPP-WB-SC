9471b61cbde9ee5e8da7caa8db942427e597c6e9
SolanoJr <solanojr@example.com>
2026-06-19 16:20:15 -0300

    Adiciona script para verificar tabelas do banco de dados

---
+++ check_tables.js
@@ -0,0 +1,8 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./data/bot_database.db');
+
+db.all("SELECT name FROM sqlite_master WHERE type='table'", (err, rows) => {
+  if (err) console.error(err);
+  else console.log(JSON.stringify(rows));
+  db.close();
+});
